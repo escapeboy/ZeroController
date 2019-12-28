58e602837d1af4f308a8dd40497b366af32f655d
Nikola Katsarov <katsarov@gmail.com>
2019-12-28 21:34:32 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,6 @@
 
 use \Illuminate\Support\Facades\Route;
 
-Route::get('call/{class}/{method}', 'Api\CallController@call')
+Route::get('call/{class}/{method}', [\ZeroController\Controllers\Api\CallController::class, 'call'])
      ->name('api.call')
      ->middleware('auth:api');
